e4bc671514245d53ffd5bf60afeede56fc78a87b
Vishnubalu <38403733+Vishnubalu@users.noreply.github.com>
2019-04-02 17:56:06 +0530

    Update tic.py

--- tic.py
+++ tic.py
@@ -6,7 +6,7 @@ import numpy as np
 
 class Main:
     table = list([8, 3, 4, 1, 5, 9, 6, 7, 2])
-    dis = ["-", "-", "-", "-", "-", "-", "-", "-", "-"]
+    dis = ["*", "*", "*", "*", "*", "*", "*", "*", "*"]
     system = list([])
     player = list([])
     ret = list([])
@@ -26,17 +26,19 @@ def system_time():
     n = []
     if len(main.table) == len(main.system)+len(main.player):
         print(" ------- DRAW MATCHING----------")
+        return 0
     else:
         if 2 <= len(main.system):
             n = check.evaluate_user(main.system)
-        if len(n) == 1 and n[0] not in main.system and n[0] not in main.player:
-            if n[0] in main.table:
+        count = 0
+        for value in n:
+            if value in main.table and value not in main.system and value not in main.player:
                 n = n
-            else:
-                n = check.evaluate_user(main.player)
-        else:
+                count = count+1
+                break;
+        if(count == 0):
             n = check.evaluate_user(main.player)
-        entry_of_system(n)
+    entry_of_system(n)
 
 
 
@@ -45,7 +47,10 @@ def entry_of_system(n):
     while True:
         # print("while")
         if len(n) == 0:
-            o = int(random.choice(np.arange(1, 10)))
+            if(len(main.player) == 1):
+                o = int(random.choice([8,4,5,6,2]))
+            else:
+                o = int(random.choice(np.arange(1, 9)))
             if (o not in main.system) and (o not in main.player):
                 # print("system input", o)
                 break
@@ -57,7 +62,7 @@ def entry_of_system(n):
                     break
             else:
                 while True:
-                    o = int(random.randint(1, 10))
+                    o = int(random.randint(1, 9))
                     if (o not in main.system) and (o not in main.player):
                         # print("system input", o)
                         break
@@ -76,14 +81,29 @@ def entry_of_system(n):
 def entry_of_player():
     if len(main.table) == len(main.system)+len(main.player):
         print("DRAW MATCHING")
+        return 0
     else:
-
-        x = int(input(" VIS move : "))
+        def taking_input():
+            try:
+                user_input = int(input(" VIS move : "))
+                return user_input;
+            except:
+                print("An error occured @ please insert integer value :")
+                taking_input()
+        x = taking_input()
         while True:
-            if (main.table[x - 1] not in main.system) and (main.table[x - 1] not in main.player):
-                break;
-            else:
-                x = int(input("already choosen $ VIS move : "))
+            try:
+                if (main.table[x - 1] not in main.system) and (main.table[x - 1] not in main.player):
+                    break;
+                else:
+                    print("already choosen $ ")
+                    x = taking_input()
+            except:
+                print("Please insert integer ranges from(1-9)")
+                x = taking_input()
+                break
+
+
         # print(x)
         main.player.append(main.table[x - 1])
         main.dis[x - 1] = "VIS"
@@ -94,9 +114,23 @@ def entry_of_player():
             print("*********YOU WON**********")
 
 
-check = v.Evaluate()  # type: Evaluate
+            check = v.Evaluate()  # type: Evaluate
 main = Main()
-print(" Table View : ")
-display()
-entry_of_player()
-print("vishnu")
+
+if __name__ == "__main__":
+    while True:
+        print(" Table View : ")
+        display()
+        # select_player = input("who place first : ")
+        if(input("who place first s/u: ").lower() == 'u'):
+            entry_of_player()
+        else:
+            system_time()
+        print("Do you want to play again (y/n) : ")
+        if input().lower() == 'n':
+            break;
+        main.dis = ["*", "*", "*", "*", "*", "*", "*", "*", "*"]
+        main.system = list([])
+        main.player = list([])
+        main.ret = list([])
+    print("Thanks for using")
